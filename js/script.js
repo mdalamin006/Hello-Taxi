272b771d04dd80dd9684dd3c19d0a7bce0972d45
Md. Al-Amin <145653719+mdalamin006@users.noreply.github.com>
2024-02-22 23:17:23 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 // JavaScript code 
 
+document.onkeydown=function(e)
+{
+    if(event.keyCode == 123)
+    {
+        return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode =='I'.charCodeAt(0))
+    {
+        return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode =='J'.charCodeAt(0))
+    {
+        return false;
+    }
+    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0))
+    {
+        return false;
+    }
+}
 
 AOS.init();
 // Aos
